4a520ace22539379622ad1302acdbab168d2d4ce
João Pedro <joao.pedro@example.com>
2026-09-23 18:59:24 -0300

    chore: set base relative path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: './',
   server: {
     host: true, // Expose on all local network interfaces (Wi-Fi / Ethernet)
     port: 5173,
